fc327fa17af6f65c0d281e018997fe6f0f22de4f
Eirik Skjærseth <eirik1502@gmail.com>
2020-05-01 16:38:07 +0200

    simplify SimulationResultQueue, seems to remove a bug where expansion of a SimulationResult did not happen

--- src/pages/SimulationQueuePage/SimulationResultQueue.tsx
+++ src/pages/SimulationQueuePage/SimulationResultQueue.tsx
@@ -1,5 +1,5 @@
 import { SimulationResult } from "../../services/simulation_queue/types"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import SingleQueue from "../../components/SingleQueue"
 import SolSimulationResult from "../../components/SolSimulationResult"
 
@@ -9,17 +9,28 @@ type Props = {
 }
 
 const SimulationResultQueue: React.FC<Props> = ({ simulationsResult, deleteAll }) => {
-  const [simulationsResultExpanded, setSimulationsResultExpanded] = useState<Set<string>>(new Set())
+  const [simulationsResultExpanded, setSimulationsResultExpanded] = useState<string[]>([])
 
-  const handleResultCliked = (simulationId: string) => {
-    setSimulationsResultExpanded((prevSet) => {
-      if (prevSet.has(simulationId)) {
-        prevSet.delete(simulationId)
-        return new Set(prevSet)
-      } else {
-        return new Set(prevSet.add(simulationId))
-      }
-    })
+  // remove expanded simulationResults if the corresponding simulation is not present
+  useEffect(() => {
+    const simResExpPresent = simulationsResultExpanded.filter((simResExpId) =>
+      simulationsResult.find((simRes) => simRes.simulationId === simResExpId),
+    )
+    if (simResExpPresent.length !== simulationsResultExpanded.length) {
+      setSimulationsResultExpanded(simResExpPresent)
+    }
+  }, [simulationsResult, simulationsResultExpanded])
+
+  const handleResultClicked = (simulationId: string) => {
+    const indexOfSimulationId = simulationsResultExpanded.indexOf(simulationId)
+    if (indexOfSimulationId === -1) {
+      setSimulationsResultExpanded([...simulationsResultExpanded, simulationId])
+    } else {
+      setSimulationsResultExpanded([
+        ...simulationsResultExpanded.slice(0, indexOfSimulationId),
+        ...simulationsResultExpanded.slice(indexOfSimulationId + 1),
+      ])
+    }
   }
 
   const simulationsResultElems = simulationsResult.map((simulationResult) => (
@@ -27,9 +38,11 @@ const SimulationResultQueue: React.FC<Props> = ({ simulationsResult, deleteAll }
       key={simulationResult.simulationId}
       simulationId={simulationResult.simulationId}
       simulationResult={
-        simulationsResultExpanded.has(simulationResult.simulationId) ? simulationResult : undefined
+        simulationsResultExpanded.includes(simulationResult.simulationId)
+          ? simulationResult
+          : undefined
       }
-      onClick={handleResultCliked}
+      onClick={handleResultClicked}
     />
   ))
 
